b048f40176ea4b2e480066b0c757771c992dd708
Kirill Cherkas <kirill.cherkas34@gmail.com>
2022-02-05 20:57:29 +0300

    методы

--- app/src/main/java/com/example/homework1/Function.kt
+++ app/src/main/java/com/example/homework1/Function.kt
@@ -2,9 +2,9 @@ package com.example.homework1
 
 import android.app.Activity
 
-fun sortByName(list: List<User>): List<User> = list.sortedBy { User -> User.userName }
+fun sortByName(list: List<User>) = list.sortedBy { User -> User.userName }
 
-fun faceControl(list: List<User>): List<User> = list.toMutableList().filter { User -> User.age >= 18 }
+fun faceControl(list: List<User>) = list.toMutableList().filter { User -> User.age >= 18 }
 
 
 
