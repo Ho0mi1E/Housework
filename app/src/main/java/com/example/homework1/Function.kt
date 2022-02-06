6e63d8bd9142869f9703b658ea72f9b846d93b1d
Kirill Cherkas <kirill.cherkas34@gmail.com>
2022-02-06 14:35:27 +0300

    методы

--- app/src/main/java/com/example/homework1/Function.kt
+++ app/src/main/java/com/example/homework1/Function.kt
@@ -2,4 +2,4 @@ package com.example.homework1
 
 fun sortByName(list: List<User>) = list.sortedBy { User -> User.userName }
 
-fun faceControl(list: List<User>) = list.toMutableList().filter { User -> User.age >= 18 }
+fun faceControl(list: List<User>) = list.filter { User -> User.age >= 18 }
